f02608191cc28d794e6310cc47041801d87c4bf9
허윤서 <heoyunseo2500@gmail.com>
2025-11-17 15:49:36 +0900

    2023621027 허윤서

--- src/movie/Reservation.java
+++ src/movie/Reservation.java
@@ -2,18 +2,18 @@ package movie;
 
 public class Reservation {
 
-    public boolean reserve11(Movie movie, int count) {
+    public boolean reserve1(Movie movie, int count) {
         if (movie.reserveSeat(count)) {
             System.out.println("✅ 예매 성공: " + movie.getTitle() + ", 수량: " + count);
             System.out.println("서연경");
             return true;
         } else {
-            System.out.println("❌ 예매 실패: 좌석 부족");
+            System.out.println(" 예매 완료! -허윤서 ");
             return false;
         }
     }
 
-    public void reserve1842() {
-        System.out.println("예매 완료!_이민정");
+    public void reserve1() {
+        System.out.println("예매 완료!_허윤서");
     }
 }
