2668028390cb3c6fbbb3160c389167a4380b5dae
Levin0200 <namseongcheon905@gmail.com>
2025-11-17 11:46:58 +0900

    예메완료 메시지 작성 코드 수정

--- src/movie/Reservation.java
+++ src/movie/Reservation.java
@@ -5,6 +5,7 @@ public class Reservation {
     public boolean reserve(Movie movie, int count) {
         if (movie.reserveSeat(count)) {
             System.out.println("✅ 예매 성공: " + movie.getTitle() + ", 수량: " + count);
+            System.out.println(("예메 완료! -남성천"));
             return true;
         } else {
             System.out.println("❌ 예매 실패: 좌석 부족");
