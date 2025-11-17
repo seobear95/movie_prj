b80b203d22580eed4d6b75d3da8ce4275b0ccdfd
박지수 <wltnqkr200@gmail.com>
2025-11-17 15:32:19 +0900

    박지수 1번 예매 코드 추가

--- src/movie/Reservation.java
+++ src/movie/Reservation.java
@@ -2,10 +2,10 @@ package movie;
 
 public class Reservation {
 
-    public boolean reserve11(Movie movie, int count) {
+    public boolean reserve_1(Movie movie, int count) {
         if (movie.reserveSeat(count)) {
             System.out.println("✅ 예매 성공: " + movie.getTitle() + ", 수량: " + count);
-            System.out.println("서연경");
+            System.out.println("박지수");
             return true;
         } else {
             System.out.println("❌ 예매 실패: 좌석 부족");
