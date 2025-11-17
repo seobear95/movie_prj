5519c6e4db2e5d74b239b47a0f13c82f2edf9401
star057 <starplayer057@gmail.com>
2025-11-17 15:32:23 +0900

    김예찬 6 예매 코드 추가

--- src/movie/Reservation.java
+++ src/movie/Reservation.java
@@ -2,18 +2,14 @@ package movie;
 
 public class Reservation {
 
-    public boolean reserve11(Movie movie, int count) {
+    public boolean reserve_6(Movie movie, int count) {
         if (movie.reserveSeat(count)) {
             System.out.println("✅ 예매 성공: " + movie.getTitle() + ", 수량: " + count);
-            System.out.println("서연경");
+            System.out.println("김예찬");
             return true;
         } else {
             System.out.println("❌ 예매 실패: 좌석 부족");
             return false;
         }
-    }
-
-    public void reserve1842() {
-        System.out.println("예매 완료!_이민정");
     }
 }
